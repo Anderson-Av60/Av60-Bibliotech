315bc0d8da9bab7bf582007c86fce1cb840366e4
Anderson Ribeiro Da Silva <wistonlaele@gmail.com>
2026-09-08 14:26:20 -0300

    atualizado de novo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login';
+import { HomeComponent } from './features/home/home';
+import { ConfiguracoesComponent } from './configuracoes/configuracoes';
+import { CategoriasComponent } from './categorias/categorias';
+import { MeusLivrosComponent } from './meus-livros/meus-livros';
+import { PerfilComponent } from './perfil/perfil';
+import { DesafiosComponent } from './desafios/desafios';
 
-export const routes: Routes = [];
+
+export const routes: Routes=[
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent },
+    {path: 'home', component: HomeComponent },
+    {path: 'configuracoes', component: ConfiguracoesComponent },
+    {path: 'categorias', component: CategoriasComponent},
+    {path: 'meus-livros', component: MeusLivrosComponent},
+    {path: 'perfil', component: PerfilComponent},
+    {path: 'desafios', component: DesafiosComponent}
+];
